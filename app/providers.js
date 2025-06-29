96e51e8264d97822f2139dbc43fd135cebe026d3
SerialGuy <mufaddaldalalmine@gmail.com>
2025-06-30 00:40:21 +0530

    Updating and Fixing deployment issue

--- app/providers.js
+++ app/providers.js
@@ -2,14 +2,12 @@
 
 import { AuthProvider } from './contexts/AuthContext';
 import { SocketProvider } from './contexts/SocketContext';
-import { Toaster } from 'react-hot-toast';
 
 export function Providers({ children }) {
   return (
     <AuthProvider>
       <SocketProvider>
         {children}
-        <Toaster position="top-right" />
       </SocketProvider>
     </AuthProvider>
   );
